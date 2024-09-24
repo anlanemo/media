8b278b59746fdb451c56b0ac1766141c055dab97
tokii <mottchenz@gmail.com>
2024-09-24 18:27:35 +0800

    Add support from Apple ProRes Video (ap4x/ap4h/apch/apcn/apcs/apco) through FfmpegDecoder.
    (cherry picked from commit f01a8aa4446bd3bcf41b167ef9e5bad7d9a7f300)

--- libraries/extractor/src/main/java/androidx/media3/extractor/mp4/Atom.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/mp4/Atom.java
@@ -428,6 +428,22 @@ import java.util.List;
   @SuppressWarnings("ConstantCaseForConstants")
   public static final int TYPE_mdcv = 0x6d646376;
 
+  @SuppressWarnings("ConstantCaseForConstants")
+  public static final int TYPE_ap4x = 0x61703468;
+
+  @SuppressWarnings("ConstantCaseForConstants")
+  public static final int TYPE_apch = 0x61706368;
+
+  @SuppressWarnings("ConstantCaseForConstants")
+  public static final int TYPE_apcn = 0x6170636e;
+
+  @SuppressWarnings("ConstantCaseForConstants")
+  public static final int TYPE_apcs = 0x61706373;
+
+  @SuppressWarnings("ConstantCaseForConstants")
+  public static final int TYPE_apco = 0x6170636f;
+
+
   public final int type;
 
   public Atom(int type) {
